726fd53e7234c58bbcd248e184f57dcec6135022
Pilar Alegre <maleram052@g.educaand.es>
2026-04-20 17:25:38 +0200

    primero

---
+++ Assets/Scripts/Targets.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Targets : MonoBehaviour
+{
+    public void Encontrado()
+    {
+        Debug.Log("Encontrado");
+    }
+
+    public void Perdido()
+    {
+        Debug.Log("Perdido");
+    }
+}
